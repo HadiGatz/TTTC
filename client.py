7f51d495c3f4015471a5f232c8fdaed5c463c04d
Hadi Gatz <albertsviridovski@gmail.com>
2024-10-18 14:57:24 +0300

    [FIX] fixed issues regarding reconnection to the server

--- client.py
+++ client.py
@@ -9,11 +9,7 @@ manager = TTTManager(board)
 HOST = '192.168.68.104'
 PORT = 5555
 client = socket.socket()
-client.connect((HOST, PORT))
     
-data = client.recv(1024).decode()
-tile_type = 'X' if data == "FIRST" else 'O'
-
 def send_move_to_server(client, tile_type):
     chosen_tile = manager.handle_user_turn(tile_type)
     data = pickle.dumps((chosen_tile, tile_type))
@@ -27,24 +23,27 @@ while True:
     ready = input("\nAre you ready to play? [Y/N]: ").lower()
     if ready == 'y':
         connect_to_server()
+        data = client.recv(1024).decode()
+        tile_type = 'X' if data == "FIRST" else 'O'
+        
+        while True:
+            command = client.recv(1024).decode()
 
-        command = client.recv(1024).decode()
+            current_board = client.recv(1024).decode()
+            print(current_board)
 
-        current_board = client.recv(1024).decode()
-        print(current_board)
+            if command == "MOVE":
+                print("\nYour move\n")
+                send_move_to_server(client, tile_type)
+            else:
+                print("\nWaiting for your opponent's move...\n")
 
-        if command == "MOVE":
-            print("\nYour move\n")
-            send_move_to_server(client, tile_type)
-        else:
-            print("\nWaiting for your opponent's move...\n")
+            current_board = client.recv(1024).decode()
+            print(current_board)
 
-        current_board = client.recv(1024).decode()
-        print(current_board)
-
-        current_game_state = client.recv(1024).decode()
-        if current_game_state != "NO_RESULT":
-            break
+            current_game_state = client.recv(1024).decode()
+            if current_game_state != "NO_RESULT":
+                break
     
     else:
         break
